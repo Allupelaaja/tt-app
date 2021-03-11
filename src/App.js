56e0ef16f8b23af208dc4da5824cc79d5961ba23
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2021-03-11 18:14:47 +0200

    simple layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createMuiTheme, responsiveFontSizes, makeStyles, ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { AppBar, Toolbar, Container } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function App() {
+const App = React.forwardRef((props, ref) => {
+  let customTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  })
+  customTheme = responsiveFontSizes(customTheme)
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+      textAlign: "center",
+    },
+    mainContent: {
+      [theme.breakpoints.up('sm')]: {
+        width: '75%',
+        margin: '0 auto',
+        padding: '30px',
+      },
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h4" className={classes.title}>
+              Timetables App
+          </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <Container className={classes.mainContent}>
+            <div>
+              <Typography variant="h6" className={classes.title}>
+                Select locations
+              </Typography>
+            </div>
+          </Container>
+        </main>
+      </div>
+    </ThemeProvider>
   );
-}
+})
 
 export default App;
