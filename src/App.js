994a72859e4119e8a6268579db3867c14204523c
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2021-03-11 18:46:26 +0200

    first graphql test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,37 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import { ApolloClient, InMemoryCache, gql, ApolloProvider, useQuery } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+  cache: new InMemoryCache()
+});
+
+const SINGLE_STOP = gql`
+  query getStop {
+    stop(id: "HSL:1040129") {
+      name
+      lat
+      lon
+    }
+  }`;
+
+function SingleStop() {
+  const { loading, error, data } = useQuery(SINGLE_STOP);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div key={data.stop.name}>
+      <p>
+        {data.stop.name}: {data.stop.lat}, {data.stop.lon}
+      </p>
+    </div>
+  );
+}
+
 const App = React.forwardRef((props, ref) => {
   let customTheme = createMuiTheme({
     palette: {
@@ -36,27 +67,30 @@ const App = React.forwardRef((props, ref) => {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <div className="App">
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h4" className={classes.title}>
-              Timetables App
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <div className="App">
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h4" className={classes.title}>
+                Timetables App
           </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          <Container className={classes.mainContent}>
-            <div>
-              <Typography variant="h6" className={classes.title}>
-                Select locations
-              </Typography>
-            </div>
-          </Container>
-        </main>
-      </div>
-    </ThemeProvider>
+            </Toolbar>
+          </AppBar>
+          <main>
+            <Container className={classes.mainContent}>
+              <div>
+                <Typography variant="h6" className={classes.title}>
+                  Select locations
+                  <SingleStop></SingleStop>
+                </Typography>
+              </div>
+            </Container>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 })
 
